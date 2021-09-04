c628040aeb1ea61230de2dd1163e4a20bd5b9ad6
sppeeedd <qustaine@gmail.com>
2021-09-04 08:34:40 +0300

    Added islands and teleport to another scenes

---
+++ Assets/Scripts/Environment/IslandTeleport.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class IslandTeleport : MonoBehaviour
+{
+    public string sceneName;
+    public float distance;
+
+    private float colorA = 0.2f;
+
+    private Image image;
+
+    private void Start()
+    {
+        image = GetComponent<Image>();
+    }
+
+    private void Update()
+    {
+        if (Vector3.Distance(Input.mousePosition, transform.position) <= distance)
+        {
+            image.color = new Color(image.color.r, image.color.g, image.color.b, colorA);
+            image.color = new Color(image.color.r, image.color.g, image.color.b, colorA);
+            if (Input.GetMouseButtonDown(0))
+            {
+                GameManager.Instance.travelMenuAnimator.SetTrigger("hide");
+
+                GameManager.Instance.SaveState();
+                SceneManager.LoadScene(sceneName);
+            }
+        }
+        else
+        {
+            image.color = new Color(image.color.r, image.color.g, image.color.b, 0);
+        }
+    }
+}
