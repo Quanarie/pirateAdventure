0041c9e25937f67d69e7f6235685a3290b1be9ba
sppeeedd <qustaine@gmail.com>
2021-09-17 19:40:49 +0300

    Added testMenu

---
+++ Assets/Menu/PauseMenuScript.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuScript : MonoBehaviour
+{
+    public GameObject pauseMenuUI;
+
+    public static bool isGamePaused = false;
+
+
+    private void Start()
+    {
+        pauseMenuUI.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isGamePaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isGamePaused = false;
+    }
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isGamePaused = true;
+    }
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Save()
+    {
+        GameManager.Instance.SaveState();
+    }
+}
